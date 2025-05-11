3988574fd49f347c2ca8ee21111242f1985df0a4
shaoniaogong <shaoniaogong@outlook.com>
2025-05-11 20:46:46 +1200

    updates

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "tfstaterg"
-    storage_account_name  = "autdemotfstate"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
   subscription_id = "57480482-27fc-46a6-8643-ee45484365ec"
@@ -14,20 +5,37 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  location = var.location
-  name     = var.resource_group_name
+  name     = "AUT-2025-demo"
+  location = "Australia East"
 }
 
-module "storage" {
-  source              = "./modules"
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_storage_account" "storage" {
+  name                     = "autdemostorage1234"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "autdemo-function-plan"
   location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
-module "function" {
-  source                 = "./modules"
-  resource_group_name    = azurerm_resource_group.rg.name
-  location               = azurerm_resource_group.rg.location
-  storage_account_name   = module.storage.azurerm_storage_account.storage.name
-  storage_account_key    = module.storage.azurerm_storage_account.storage.primary_access_key
+resource "azurerm_linux_function_app" "function" {
+  name                       = "autdemo-functionapp1234"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  service_plan_id            = azurerm_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+
+  site_config {
+    application_stack {
+      node_version = "18"
+    }
+  }
 }
--- modules/st.tf
+++ modules/st.tf
@@ -5,3 +5,11 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "storage_account_key" {
+  value = azurerm_storage_account.storage.primary_access_key
+}
