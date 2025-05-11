e978a61fde5a3130050f25db5d75026153e18c2a
jamesyoung007 <jamesyoung007@gmail.com>
2025-05-11 20:26:56 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstaterg"
+    storage_account_name  = "autdemotfstate"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "57480482-27fc-46a6-8643-ee45484365ec"
