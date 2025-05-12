78be5753f26acfb3f1889263638551de7adadb33
shaoniaogong <shaoniaogong@outlook.com>
2025-05-12 19:59:21 +1200

    updates

--- main.tf
+++ main.tf
@@ -13,79 +13,35 @@ provider "azurerm" {
   resource_provider_registrations = "none"
 }
 
-variable "location" {
-  type    = string
-  default = "Australia East"
-}
-
-variable "resource_group_name" {
-  type    = string
-  default = "AUT-2025-demo"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "autdemostorage1234"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_service_plan" "plan" {
-  name                = "autdemo-function-plan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "Y1"
-}
-
-resource "azurerm_linux_function_app" "function" {
-  name                       = "autdemo-functionapp1234"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  service_plan_id            = azurerm_service_plan.plan.id
-  storage_account_name       = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-
-  site_config {
-    application_stack {
-      node_version = "18"
-    }
-  }
-}
-
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "autdemo-law"
+module "storage" {
+  source              = "./modules"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
 }
 
-resource "azurerm_monitor_diagnostic_setting" "storage_diagnostics" {
-  name                       = "diag-storage"
-  target_resource_id         = azurerm_storage_account.storage.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+module "function" {
+  source                    = "./modules"
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  storage_account_name      = module.storage.storage_account_name
+  storage_account_access_key = module.storage.storage_account_access_key
+}
 
-  metric {
-    category = "Transaction"
-    enabled  = true
-
-    retention_policy {
-      enabled = false
-    }
-  }
+module "monitoring" {
+  source              = "./modules/monitoring"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_monitor_diagnostic_setting" "function_diagnostics" {
   name                       = "diag-function"
-  target_resource_id         = azurerm_linux_function_app.function.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  target_resource_id         = module.function.function_app_id
+  log_analytics_workspace_id = module.monitoring.log_analytics_workspace_id
 
   enabled_log {
     category = "FunctionAppLogs"
@@ -96,6 +52,20 @@ resource "azurerm_monitor_diagnostic_setting" "function_diagnostics" {
 
   metric {
     category = "AllMetrics"
+    enabled  = true
+    retention_policy {
+      enabled = false
+    }
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "storage_diagnostics" {
+  name                       = "diag-storage"
+  target_resource_id         = module.storage.storage_account_id
+  log_analytics_workspace_id = module.monitoring.log_analytics_workspace_id
+
+  metric {
+    category = "Transaction"
     enabled  = true
     retention_policy {
       enabled = false
