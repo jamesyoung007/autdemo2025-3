ab0b28bc81f0b6fdd383e61efbc2738411b9ee76
shaoniaogong <shaoniaogong@outlook.com>
2025-05-12 20:06:43 +1200

    updates

---
+++ modules/func/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "storage_account_access_key" {
+  type = string
+}
