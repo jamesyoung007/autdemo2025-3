960880472d09c36f2f48d538eae7251e124bc973
shaoniaogong <shaoniaogong@outlook.com>
2025-05-12 20:54:56 +1200

    updated outputs

--- modules/func/outputs.tf
+++ modules/func/outputs.tf
@@ -1,3 +1,1 @@
-output "function_app_id" {
-  value = azurerm_linux_function_app.function.id
-}
+// Remove duplicate output definitions as they are already defined in main.tf
--- modules/st/outputs.tf
+++ modules/st/outputs.tf
@@ -1,11 +1,1 @@
-output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
-}
-
-output "storage_account_access_key" {
-  value = azurerm_storage_account.storage.primary_access_key
-}
-
-output "storage_account_id" {
-  value = azurerm_storage_account.storage.id
-}
+// Remove duplicate output definitions as they are already defined in main.tf
