335eea308e0f27146abc8784207e869e296b2100
shaoniaogong <shaoniaogong@outlook.com>
2025-05-13 23:10:52 +1200

    UPDATES

--- main.tf
+++ main.tf
@@ -18,6 +18,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_service_plan" "plan" {
+  name                = "autdemo2-function-plan"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
 module "storage" {
   source              = "./modules/st"
   location            = var.location
